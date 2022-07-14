3ea89e087c4edab931e6a8dff3aab4765f099aee
NK <squiblawarcurse@gmail.com>
2022-07-14 14:54:29 +0300

    final version v2

--- src/main/java/com/idf/korovin/cryptocurrencytask/service/AutomaticCurrencySaverService.java
+++ src/main/java/com/idf/korovin/cryptocurrencytask/service/AutomaticCurrencySaverService.java
@@ -10,7 +10,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @EnableScheduling
 @Service
@@ -26,23 +28,32 @@ public class AutomaticCurrencySaverService {
     private final String currencyResourceUrl = "https://api.coinlore.net/api/ticker/?id=";
 
     @Scheduled(fixedRate = 60000)
-    private void saveCurrencyPriceTry(){
+    private void saveCurrencyPriceTry() {
         try {
             saveCurrencyPrice();
-        }catch (HttpServerErrorException e){
+        } catch (HttpServerErrorException e) {
             saveCurrencyPriceTry();
         }
     }
 
     private void saveCurrencyPrice() {
-        CryptoCurrencyRoster.stream()
-                .forEach(currencyRoster -> {
-                        CryptoCurrency[] cryptoCurrencies = restTemplate
-                                .getForObject(currencyResourceUrl + currencyRoster.getId(), CryptoCurrency[].class);
-                        if (cryptoCurrencies != null) {
-                            cryptoCurrencyRepository.saveAll(Arrays.asList(cryptoCurrencies));
-                        }
-                });
+        CryptoCurrency[] cryptoCurrencies;
+        List<Long> cryptoIds = listOfCryptoIds();
+
+        cryptoCurrencies = restTemplate.getForObject(currencyResourceUrl + cryptoIds.get(0) + "," + cryptoIds.get(1) +
+                "," + cryptoIds.get(2), CryptoCurrency[].class);
+
+        if (cryptoCurrencies != null) {
+            cryptoCurrencyRepository.saveAll(Arrays.asList(cryptoCurrencies));
+        }
         priceChangeNotifierService.notifyWhenPriceHasChanged();
     }
+
+    private static List<Long> listOfCryptoIds() {
+        List<Long> cryptoIds = new ArrayList<>();
+        CryptoCurrencyRoster.stream()
+                .forEach(currencyRoster -> cryptoIds.add(currencyRoster.getId()));
+        return cryptoIds;
+    }
+
 }
